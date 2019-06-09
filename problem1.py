56b302139fa5428d6f83d4900e20f163e6e4b149
chiragjain712 <chiragjaingandhi712@gmail.com>
2019-06-10 01:42:05 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,14 @@
+
+
+from datetime import date
+name= input("Please enter your name here:--> ")
+age= input("Please enter your current age:--> ")
+#print("Hii, "+name+" your age is "+age)
+if int(age) < 95:
+    year= 95 - int(age)
+    print("You will be of 95 years in : ")
+    print (date.today().year + year)
+    
+else:
+    print("You are already "+ age)
+
